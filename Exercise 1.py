19c7f693f94a530d7da35623a1452f7fab454daf
indi06-star <indiphilemcengwa.29@gmail.com>
2024-11-08 09:58:16 +0200

    python exercise has been uploaded

---
+++ Exercise 1.py
@@ -0,0 +1,37 @@
+# Question 1: Variable Assignment and String Manipulation
+
+# TODO: Ask the user for their name and store it in a variable
+username = input("what is your name? ")
+
+# TODO: Ask the user for their age and store it in a variable
+userage  = input ("what is your age ?")
+    
+# TODO: Print a greeting using the name and age variables
+print(f"hi {username} who is{userage} + years old")
+#------------------------------------------------------------------------------------
+# Question 2: Integer Operations
+
+# TODO: Ask the user for the length of a rectangle and store it as an integer
+length = int(input("Enter the length of a rectangle"))
+
+# TODO: Ask the user for the width of a rectangle and store it as an integer
+width = int(input("Enter the width of a rectangle "))
+
+# TODO: Calculate the area of the rectangle
+area = length * width 
+
+# TODO: Print the result
+print(f"The area is: {area}")
+#------------------------------------------------------------------------------------
+# Question 3: Working with Floats
+
+# TODO: Ask the user for a temperature in Celsius and store it as a float
+celsius = float(input("enter the temperature in celsius")) 
+temperature =celsius 
+
+# TODO: Convert the temperature to Fahrenheit using the formula: (C * 9/5) + 32
+fahrenheit = (temperature * 9/5) + 32
+
+# TODO: Print the result rounded to two decimal placesprint(fahrenheit) 
+print(f"The fahrenheit is {round(fahrenheit,2)}")
+
